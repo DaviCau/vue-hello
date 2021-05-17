e87591c384a8e54dd34cc9e0c7629412510eb8ca
DaviCau <davide.cau.rn@gmail.com>
2021-05-17 16:09:14 +0200

    aggiunta immagine con i data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: "#root",
     data: {
         message: "Inserisci il messaggio che vuoi scrivere in pagina",
-        style:"color:red; font-size:50px"
-    },
+        style:"color:red; font-size:50px",
+        img: "https://www.raiplayradio.it/cropgd/853x480/dl/img/2018/04/26/1524742758393_paesaggio.jpg"
+    }
 })
